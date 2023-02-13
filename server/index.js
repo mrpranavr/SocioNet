c6f35861420da501303fda2bf88b4a3770b391ff
Pranav R <pranav81234@gmail.com>
2023-02-13 23:00:26 +0530

    posts model and controller

--- server/index.js
+++ server/index.js
@@ -1,61 +1,73 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from  'cors'
-import dotenv from 'dotenv'
-import multer from 'multer'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import {register} from './controllers/auth.js'
-import authRoutes from './routes/auth.js'
-import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts.js'
-import {createPost} from './controllers/posts.js'
-import { verifyToken } from './middleware/auth.js'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from './models/User.js'
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIGURATIONS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
 
-const app = express()
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-app.use(morgan('common'))
-app.use(bodyParser.json({limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
-app.use(cors())
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "public/assets")
+        cb(null, "public/assets");
     },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage });
 
 // ROUTES WITH FILES
-app.post('/auth/register', upload.single('picture'), register)
-app.post('/posts', verifyToken, upload.single('picture'), createPost)
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
-app.use('/auth', authRoutes)
-app.use('/users', userRoutes)
-app.use('/posts', postRoutes)
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
-const PORT = process.env.port | 6000
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server port: ${PORT}`))
-}).catch((error) => console.log(`${error} did not connect`))
+const PORT = process.env.port | 6000;
+mongoose.set("strictQuery", true);
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('listeniong')
+        app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+
+        // MOCK DATA ----- ADD THESE ONLY ONCE
+        // User.insertMany(users)
+        // Post.insertMany(posts)
+    })
+    .catch((error) => console.log(`${error} did not connect`));
