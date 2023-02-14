78672f28c823256c47e17223225c937baf4f6ff8
Pranav R <pranav81234@gmail.com>
2023-02-14 13:47:31 +0530

    client start

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,7 +6,7 @@ import { verifyToken } from '../middleware/auth.js'
 const router = express.Router()
 
 // READ
-router.get('/', verifyToken, getFeedPosts)
+router.get('/',verifyToken, getFeedPosts)
 router.get('/:userId/posts', verifyToken, getUserPosts)
 
 // UPDATE
